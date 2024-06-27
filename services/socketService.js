c2134377ef05d86aa1bff907a28fa94017f43687
ngevorgyan <gevorgyan.narek.prog@gmail.com>
2024-06-27 13:51:15 +0400

    fix

--- services/socketService.js
+++ services/socketService.js
@@ -9,9 +9,8 @@ class SocketService {
     if (!this.io) {
       this.io = new socketIO(server, {
         cors: {
-          origin: "http://localhost:5173",
-          // origin: "https://c918-5-77-206-197.ngrok-free.app",
-          // origin: "https://673d-5-77-206-197.ngrok-free.app",
+          // origin: "http://localhost:5173",
+          origin: "https://emulator-front.netlify.app",
           methods: ["GET", "POST"],
         },
       });
